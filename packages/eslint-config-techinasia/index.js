cf42542d3388bef61f3c9fa903f4c2430bee9894
Kwong How <kwonghow@gmail.com>
2016-12-20 17:32:41 +0800

    Disable `jsx-a11y/no-static-element-interactions` rule

--- packages/eslint-config-techinasia/index.js
+++ packages/eslint-config-techinasia/index.js
@@ -4,6 +4,7 @@ module.exports = {
     'eslint-config-techinasia-base'
   ].map(require.resolve),
   rules: {
+    'jsx-a11y/no-static-element-interactions': 0,
     'react/forbid-prop-types': 0,
     'react/prefer-stateless-function': 1
   }
