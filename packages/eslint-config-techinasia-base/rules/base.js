0a47e644244abaa5bce342fa282184f8eff8c514
Kwong How <kwonghow@gmail.com>
2016-12-20 11:23:56 +0800

    Disable `no-underscore-dangle` rule

--- packages/eslint-config-techinasia-base/rules/base.js
+++ packages/eslint-config-techinasia-base/rules/base.js
@@ -4,6 +4,7 @@ module.exports = {
     'comma-dangle': 0,
     indent: [2, 2, { SwitchCase: 1 }],
     'no-param-assign': 0,
+    'no-underscore-dangle': 0,
     'max-len': [1, 100, 4, {
       ignoreComments: true,
       ignoreUrls: true
